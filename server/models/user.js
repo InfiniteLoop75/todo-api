970aea12f80e8f08f4a11b12ed03e82c833f6a81
saydakhmatov ibrokhimjon <ibrohim95.tashkent@gmail.com>
2017-12-07 19:15:24 +0500

    Add POST /todos route.

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,4 @@
-const {mongoose} = require('mongoose');
+const mongoose = require('mongoose');
 var User = mongoose.model('User', {
     email: {
         type: String,
--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const {mongoose} = require('./db/mongoose');
-const {Todo} = request('./models/todo');
-const {User} = request('./models/user');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+const server = express();
+
+server.use(bodyParser.json());
+server.post('/todos', (req, res)=>{
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc)=>{
+        res.send(doc);
+    }, (e)=>{
+        res.status(400).send(e);
+    });
+});
+server.listen(3000, ()=>{
+    console.log('Started on PORT 3000');
+});
 
 
