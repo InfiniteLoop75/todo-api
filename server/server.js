375edf2b76305c7f6ebbb61f70b5df0e48621f8f
saydakhmatov ibrokhimjon <ibrohim95.tashkent@gmail.com>
2017-12-07 18:29:14 +0500

    Server configuration

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = request('./models/todo');
+const {User} = request('./models/user');
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-//save new something
+
